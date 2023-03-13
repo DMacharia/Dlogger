2cdaa5e6d7eaaeaaa7e5def46edb2b13fbf3f381
Daniel Macharia <danielhuios@gmail.com>
2023-03-13 23:12:02 +0300

    :art: defines initial routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { ContactUsComponent } from './components/pages/contact-us/contact-us.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SingleCategoryComponent } from './components/pages/single-category/single-category.component';
+import { SinglePostComponent } from './components/pages/single-post/single-post.component';
+import { TermsAndConditionsComponent } from './components/pages/terms-and-conditions/terms-and-conditions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'category', component:SingleCategoryComponent},
+  {path: 'post', component:SinglePostComponent},
+
+  {path: 'about', component:AboutUsComponent},
+  {path: 'contact', component:ContactUsComponent},
+  {path: 'terms-and-conditions', component:TermsAndConditionsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
