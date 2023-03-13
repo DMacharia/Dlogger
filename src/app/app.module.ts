913ffaf0173719d813191d907b0f534ec85496cd
Daniel Macharia <danielhuios@gmail.com>
2023-03-13 23:11:31 +0300

    :art: declares the initial project components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { CategoryNavbarComponent } from './components/layouts/category-navbar/category-navbar.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SingleCategoryComponent } from './components/pages/single-category/single-category.component';
+import { SinglePostComponent } from './components/pages/single-post/single-post.component';
+import { TermsAndConditionsComponent } from './components/pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactUsComponent } from './components/pages/contact-us/contact-us.component';
+import { SubscriptionFormComponent } from './components/subscription-form/subscription-form.component';
+import { CommentsFormComponent } from './components/comments/comments-form/comments-form.component';
+import { CommentsListComponent } from './components/comments/comments-list/comments-list.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoryNavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    TermsAndConditionsComponent,
+    ContactUsComponent,
+    SubscriptionFormComponent,
+    CommentsFormComponent,
+    CommentsListComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
